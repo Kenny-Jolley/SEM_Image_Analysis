601b1f2da82a60c25303f60a802ba782e6283c62
Kenny Jolley <kenny.jolley@gmail.com>
2020-05-13 17:06:36 +0100

    Update Analyse_Images.py

--- Analyse_Images.py
+++ Analyse_Images.py
@@ -3,6 +3,7 @@
 # Example code for calling the analysis functions.
 
 from TEM_Image_Analysis_Milled_Line_Detect import tem_image_analysis_milled_line_detect
+from TEM_Image_Analysis_Depo_Line_Detect import tem_image_analysis_depo_line_detect
 
 
 filename = "Example_Data/Milled_Images/Gilsocarbon/40Cup.tif"
@@ -49,3 +50,62 @@ tem_image_analysis_milled_line_detect(verbose=True,
                                       crop_left=100,             # pixels to crop from the left
                                       crop_right=100,            # pixels to crop from the right
                                       total_width_cols=4000)     # image width about centre to use for horizontal line
+
+
+
+filename = "Example_Data/Deposited_Images/Copper/01.tif"
+
+tem_image_analysis_depo_line_detect(verbose=True,
+                                    filename=filename,         # filename
+                                    img_width=18.0,            # Real image width in microns
+                                    crop_top=200,             # pixels to crop from the top
+                                    crop_bottom=300,          # pixels to crop from the bottom
+                                    crop_left=10,             # pixels to crop from the left
+                                    crop_right=10,            # pixels to crop from the right
+                                    total_width_cols=1500)     # image width about centre to use for horizontal line
+
+filename = "Example_Data/Deposited_Images/Copper/02.tif"
+
+tem_image_analysis_depo_line_detect(verbose=True,
+                                    filename=filename,         # filename
+                                    img_width=18.0,            # Real image width in microns
+                                    crop_top=200,             # pixels to crop from the top
+                                    crop_bottom=300,          # pixels to crop from the bottom
+                                    crop_left=10,             # pixels to crop from the left
+                                    crop_right=10,            # pixels to crop from the right
+                                    total_width_cols=1500)     # image width about centre to use for horizontal line
+
+
+filename = "Example_Data/Deposited_Images/Copper_Graphite/01_001.tif"
+
+tem_image_analysis_depo_line_detect(verbose=True,
+                                    filename=filename,         # filename
+                                    img_width=24.0,            # Real image width in microns
+                                    crop_top=50,             # pixels to crop from the top
+                                    crop_bottom=150,          # pixels to crop from the bottom
+                                    crop_left=100,             # pixels to crop from the left
+                                    crop_right=10,            # pixels to crop from the right
+                                    total_width_cols=1500)     # image width about centre to use for horizontal line
+
+
+filename = "Example_Data/Deposited_Images/Gilsocarbon/10 kv w dep_063.tif"
+
+tem_image_analysis_depo_line_detect(verbose=True,
+                                    filename=filename,         # filename
+                                    img_width=7.5,            # Real image width in microns
+                                    crop_top=50,             # pixels to crop from the top
+                                    crop_bottom=150,          # pixels to crop from the bottom
+                                    crop_left=100,             # pixels to crop from the left
+                                    crop_right=10,            # pixels to crop from the right
+                                    total_width_cols=1500)     # image width about centre to use for horizontal line
+
+filename = "Example_Data/Deposited_Images/Gilsocarbon/10 kv w dep_068.tif"
+
+tem_image_analysis_depo_line_detect(verbose=True,
+                                    filename=filename,         # filename
+                                    img_width=7.5,            # Real image width in microns
+                                    crop_top=50,             # pixels to crop from the top
+                                    crop_bottom=150,          # pixels to crop from the bottom
+                                    crop_left=100,             # pixels to crop from the left
+                                    crop_right=10,            # pixels to crop from the right
+                                    total_width_cols=1500)     # image width about centre to use for horizontal line
